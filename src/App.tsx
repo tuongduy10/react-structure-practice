c3c4b4d13dc892c28368a0212fc936600794a21e
tuongduy10 <vtngduy101120@gmail.com>
2022-10-19 17:48:56 +0700

    test route

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense} from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './features/home/index';
+
+// const Home = React.lazy(() => import('./features/home/index'))
+// const About = React.lazy(() => import('./features/about-us'))
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Routes>
+        {/* <Route index path='/' element={Home} /> */}
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
